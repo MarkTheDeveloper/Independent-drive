1c41178a66404d39ccec1b697c2946995d66f0ad
Huy <huynh.huy@student.greenriver.edu>
2025-05-25 11:14:07 -0700

    small adjustments'

--- data/save_custom_fields.php
+++ data/save_custom_fields.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json'); // để trình duyệt hiểu là JSON
+header('Content-Type: application/json'); 
 
 $data = json_decode(file_get_contents("php://input"), true);
 
